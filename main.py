693dca08894889243812bd9994535a8371c6f46a
Cem Onyuksel <cem.onyuksel@cfa.harvard.edu>
2023-06-27 15:59:38 -0400

    most camera settings implemented

--- main.py
+++ main.py
@@ -17,75 +17,28 @@ class App(tk.Tk):
         self.make_frames()
         self.create_devices()
         self.start_tasks()
+        self.hydrate()
 
     def set_defaults(self):
         """Set application defaults"""
         self.view_delay = 1000 // 60 # 60 Hz
         self.def_padding = "3 3 12 12"
         self.check_resolution = False
+        self.camera_info1 = tk.StringVar(self, value="")
+        self.camera_info2 = tk.StringVar(self, value="")
+        self.img_props = tk.StringVar(self, "")
 
     def make_frames(self):
         """Make view frames"""
 
-        # make camera control frame
-        cam_ctrl = ttk.Frame(self, padding=self.def_padding)
-        cam_ctrl.grid(column=0, row=0)
-        self.camera_run_mode = tk.IntVar(value=Camera.NORMAL)
-        self.camera_bits = tk.IntVar(value=8)
-        self.camera_resolution1 = tk.StringVar(value="1280")
-        self.camera_resolution2 = tk.StringVar(value="960")
-        self.camera_bin_mode = tk.IntVar(value=Camera.NO_BIN)
-        ttk.Label(cam_ctrl, text="Camera Settings").grid(columnspan=4, row=0)
-        ttk.Label(cam_ctrl, text="Mode").grid(column=0, row=1, sticky=tk.E, padx=10)
-        ttk.Radiobutton(cam_ctrl, text="Stream", value=Camera.NORMAL,
-                        variable=self.camera_run_mode,
-                        command=self.set_cam_ctrl).grid(column=1, row=1, sticky=tk.W)
-        ttk.Radiobutton(cam_ctrl, text="Trigger", value=Camera.TRIGGER,
-                        variable=self.camera_run_mode,
-                        command=self.set_cam_ctrl).grid(column=2, row=1, sticky=tk.W)
-        ttk.Button(cam_ctrl, text="Snap", command=self.snap_img,
-                   width=4).grid(column=3, row=1, sticky=tk.W)
-        ttk.Radiobutton(cam_ctrl, text="8 bit", value=8,
-                        variable=self.camera_bits,
-                        command=self.set_cam_ctrl).grid(column=1, row=2, sticky=tk.W)
-        ttk.Radiobutton(cam_ctrl, text="12 bit", value=12,
-                        variable=self.camera_bits,
-                        command=self.set_cam_ctrl, # TODO add 12-bit
-                        state=tk.DISABLED).grid(column=2, row=2, sticky=tk.W)
-        ttk.Label(cam_ctrl, text="Resolution").grid(column=0, row=3,
-                                                    sticky=tk.E, padx=10)
-        ttk.Entry(cam_ctrl, width=5, textvariable=self.camera_resolution1,
-                  validatecommand=(self.register(self.valid_resolution), '%P'),
-                  validate='all').grid(column=1, row=3, sticky=tk.E)
-        ttk.Entry(cam_ctrl, width=5,textvariable=self.camera_resolution2,
-                  validatecommand=(self.register(self.valid_resolution), '%P'),
-                  validate='all').grid(column=2, row=3, sticky=tk.W)
-        ttk.Button(cam_ctrl, text="Set", command=self.set_cam_ctrl,
-                   width=4).grid(column=3, row=3, sticky=tk.W)
-        ttk.Label(cam_ctrl, text="Binning").grid(column=0, row=4,
-                                                 sticky=tk.E, padx=10)
-        ttk.Radiobutton(cam_ctrl, text="No Bin", value=Camera.NO_BIN,
-                        variable=self.camera_bin_mode,
-                        command=self.set_cam_ctrl).grid(column=1, row=4, sticky=tk.W)
-        ttk.Radiobutton(cam_ctrl, text="1:2", value=Camera.BIN1X2,
-                        variable=self.camera_bin_mode,
-                        command=self.set_cam_ctrl).grid(column=1, row=5, sticky=tk.W)
-        ttk.Radiobutton(cam_ctrl, text="1:3", value=Camera.BIN1X3,
-                        variable=self.camera_bin_mode,
-                        command=self.set_cam_ctrl).grid(column=2, row=5, sticky=tk.W)
-        ttk.Radiobutton(cam_ctrl, text="1:4", value=Camera.BIN1X4,
-                        variable=self.camera_bin_mode,
-                        command=self.set_cam_ctrl).grid(column=1, row=6, sticky=tk.W)
-        ttk.Radiobutton(cam_ctrl, text="1:4 skip", value=Camera.SKIP,
-                        variable=self.camera_bin_mode,
-                        command=self.set_cam_ctrl).grid(column=2, row=6, sticky=tk.W)
+        self.make_camera_control_frame()
 
         # make image viewer frame
         viewer = ttk.Frame(self, padding=self.def_padding)
         viewer.grid(column=1, row=0)
         self.preview = ttk.Label(viewer)
-        self.preview.grid(column=0, row=0)
-        ttk.Label(viewer, text="properties").grid(column=0, row=1)
+        self.preview.grid(column=0, row=0, columnspan=2, sticky=tk.W)
+        ttk.Label(viewer, textvariable=self.img_props).grid(column=0, row=2, sticky=tk.W)
 
         # make motion control frame
         motion = ttk.Frame(self, padding=self.def_padding)
@@ -94,11 +47,111 @@ class App(tk.Tk):
         ttk.Label(motion, text="Y").grid(column=0, row=1)
         ttk.Label(motion, text="Z").grid(column=0, row=2)
 
+    def make_camera_control_frame(self):
+        """Make camera control frame"""
+
+        cam_ctrl = ttk.LabelFrame(self, text="Camera Control",
+                                  labelanchor=tk.N, padding=self.def_padding)
+        cam_ctrl.grid(column=0, row=0, sticky=tk.N)
+
+        self.camera_run_mode = tk.IntVar(value=Camera.NORMAL)
+        self.camera_bits = tk.IntVar(value=8)
+        self.camera_resolution1 = tk.StringVar(value="1280")
+        self.camera_resolution2 = tk.StringVar(value="960")
+        self.camera_bin_mode = tk.IntVar(value=Camera.NO_BIN)
+        self.camera_exp_t = tk.StringVar(value="50.0")
+        self.camera_fps = tk.StringVar(value="10.0")
+        self.camera_gain = tk.StringVar(value="15")
+
+        # camera info
+        ttk.Label(cam_ctrl, text="Model").grid(column=0, row=0, sticky=tk.E, padx=10)
+        ttk.Label(cam_ctrl, textvariable=self.camera_info1).grid(column=1, row=0,
+                                                                 columnspan=2, sticky=tk.W)
+        ttk.Label(cam_ctrl, text="S/N").grid(column=0, row=1, sticky=tk.E, padx=10)
+        ttk.Label(cam_ctrl, textvariable=self.camera_info2).grid(column=1, row=1,
+                                                                 columnspan=2, sticky=tk.W)
+
+        # camera mode
+        ttk.Label(cam_ctrl, text="Mode").grid(column=0, row=2, sticky=tk.E, padx=10)
+        ttk.Radiobutton(cam_ctrl, text="Stream", value=Camera.NORMAL,
+                        variable=self.camera_run_mode,
+                        command=self.set_cam_ctrl).grid(column=1, row=2, sticky=tk.W)
+        ttk.Radiobutton(cam_ctrl, text="Trigger", value=Camera.TRIGGER,
+                        variable=self.camera_run_mode,
+                        command=self.set_cam_ctrl).grid(column=2, row=2, sticky=tk.W)
+        ttk.Radiobutton(cam_ctrl, text="8 bit", value=8,
+                        variable=self.camera_bits,
+                        command=self.set_cam_ctrl).grid(column=1, row=3, sticky=tk.W)
+        ttk.Radiobutton(cam_ctrl, text="12 bit", value=12,
+                        variable=self.camera_bits,
+                        command=self.set_cam_ctrl, # TODO add 12-bit
+                        state=tk.DISABLED).grid(column=2, row=3, sticky=tk.W)
+        
+        # resolution
+        ttk.Label(cam_ctrl, text="Resolution").grid(column=0, row=4,
+                                                    sticky=tk.E, padx=10)
+        ttk.Entry(cam_ctrl, width=5, textvariable=self.camera_resolution1,
+                  validatecommand=(self.register(self.valid_resolution), '%P'),
+                  validate='all').grid(column=1, row=4, sticky=tk.E)
+        ttk.Entry(cam_ctrl, width=5, textvariable=self.camera_resolution2,
+                  validatecommand=(self.register(self.valid_resolution), '%P'),
+                  validate='all').grid(column=2, row=4, sticky=tk.W)
+
+        # binning
+        ttk.Label(cam_ctrl, text="Binning").grid(column=0, row=5,
+                                                 sticky=tk.E, padx=10)
+        ttk.Radiobutton(cam_ctrl, text="No Bin", value=Camera.NO_BIN,
+                        variable=self.camera_bin_mode,
+                        command=self.set_cam_ctrl).grid(column=1, row=5,
+                                                        columnspan=2,
+                                                        sticky=tk.W)
+        ttk.Radiobutton(cam_ctrl, text="1:2", value=Camera.BIN1X2,
+                        variable=self.camera_bin_mode,
+                        command=self.set_cam_ctrl).grid(column=1, row=6, sticky=tk.W)
+        ttk.Radiobutton(cam_ctrl, text="1:3", value=Camera.BIN1X3,
+                        variable=self.camera_bin_mode,
+                        command=self.set_cam_ctrl).grid(column=2, row=6, sticky=tk.W)
+        ttk.Radiobutton(cam_ctrl, text="1:4", value=Camera.BIN1X4,
+                        variable=self.camera_bin_mode,
+                        command=self.set_cam_ctrl).grid(column=1, row=7, sticky=tk.W)
+        ttk.Radiobutton(cam_ctrl, text="1:4 skip", value=Camera.SKIP,
+                        variable=self.camera_bin_mode,
+                        command=self.set_cam_ctrl).grid(column=2, row=7,
+                                                        columnspan=2,
+                                                        sticky=tk.W)
+        
+        # exposure time
+        ttk.Label(cam_ctrl, text="Exp. Time (ms)").grid(column=0, row=8, sticky=tk.E, padx=10)
+        ttk.Entry(cam_ctrl, width=5, textvariable=self.camera_exp_t,
+                  validatecommand=(self.register(self.valid_float), '%P'),
+                  validate='all').grid(column=1, row=8, sticky=tk.W)
+
+        # FPS
+        ttk.Label(cam_ctrl, text="Frames per Sec").grid(column=0, row=9,
+                                                        sticky=tk.E, padx=10)
+        ttk.Entry(cam_ctrl, width=5, textvariable=self.camera_fps,
+                  validatecommand=(self.register(self.valid_float), '%P'),
+                  validate='all').grid(column=1, row=9, sticky=tk.W)
+
+        # gain
+        ttk.Label(cam_ctrl, text="Gain [6-41]dB").grid(column=0, row=10,
+                                                       sticky=tk.E, padx=10)
+        ttk.Entry(cam_ctrl, width=5, textvariable=self.camera_gain,
+                  validatecommand=(self.register(self.valid_resolution), '%P'),
+                  validate='all').grid(column=1, row=10, sticky=tk.W)
+
+        # buttons
+        ttk.Button(cam_ctrl, text="Take Image",
+                   command=self.snap_img).grid(column=0, row=11, pady=(10, 0), padx=10)
+        ttk.Button(cam_ctrl, text="Write to Camera",
+                   command=self.set_cam_ctrl).grid(column=1, row=11, columnspan=2,
+                                                   sticky=tk.E, pady=(10, 0), padx=10)
+
+
     def create_devices(self):
         """Create device handles"""
         try:
             self.camera = Camera()
-            self.camera.print_introduction()
         except ValueError as e:
             print(e)
             self.camera = None
@@ -106,6 +159,14 @@ class App(tk.Tk):
     def start_tasks(self):
         """Start cyclic tasks."""
         self.preview.after(self.view_delay, self.update_preview)
+
+    def hydrate(self):
+        """Fill in UI elements"""
+        if self.camera:
+            info = self.camera.get_camera_info()
+            self.camera_info1.set(info["ModuleNo"].strip('\0')) # type: ignore
+            self.camera_info2.set(info["SerialNo"].strip('\0')) # type: ignore
+
 
     def update_preview(self):
         """Update preview image in viewer"""
@@ -120,6 +181,12 @@ class App(tk.Tk):
                                                                 frame_img.height // 4)))
                 self.preview.img = disp_img # protect from garbage collect
                 self.preview.configure(image=disp_img)
+
+                # update img_props
+                prop_str = self.img_props.get()
+                prop_str = ""
+                prop_str = prop_str + str(frame.frameTime)
+                self.img_props.set(prop_str)
 
                 # update UI to proper values, matching newest frame
                 if self.check_resolution:
@@ -141,10 +208,23 @@ class App(tk.Tk):
             self.camera.set_resolution(resolution=resolution,
                                        bin_mode=self.camera_bin_mode.get(),
                                        write_now=True)
+            self.camera.set_exposure_time(exposure_time=float(self.camera_exp_t.get()),
+                                          write_now=True)
+            self.camera.set_fps(fps=float(self.camera_fps.get()),
+                                write_now=True)
+            self.camera.set_gain(gain=int(self.camera_gain.get()),
+                                 write_now=True)
+            
             # throw out old frames
             self.camera.clear_buffer()
+
             # tell update_preview to also update the resolution
             self.check_resolution = True
+
+            # read back validated settings from camera
+            self.camera_exp_t.set(str(self.camera.exposure_time))
+            self.camera_fps.set(str(self.camera.fps))
+            self.camera_gain.set(str(self.camera.gain))
 
     def snap_img(self):
         """Snap and image"""
@@ -158,6 +238,14 @@ class App(tk.Tk):
             if int.bit_length(res) <= 16:
                 return True
         return False
+    
+    def valid_float(self, f_str : str):
+        """Check if a float value is valid"""
+        try:
+            float(f_str)
+            return True
+        except:
+            return False
 
 
 if __name__ == "__main__":
