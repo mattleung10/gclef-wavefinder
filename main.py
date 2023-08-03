dbf0705a0ca820553e42d2bb882005b6df5bfc84
Cem Onyuksel <cem.onyuksel@cfa.harvard.edu>
2023-08-03 13:21:05 -0400

    add command line config file input, minimal config

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import argparse
+
 from wavefinder.gui.app import App
 
 if __name__ == "__main__":
-    app = App(config_file="config.toml")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_file", nargs='?', default="config.toml",
+                        help="TOML config file. See config.toml for more info.")
+    cf = parser.parse_args().config_file
+    app = App(cf)
 
 
 ### TODO list
