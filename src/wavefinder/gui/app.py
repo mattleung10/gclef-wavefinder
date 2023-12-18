bcfba0600ee00b81108f591ca8d540e81d583cd9
Cem Onyuksel <cem.onyuksel@cfa.harvard.edu>
2023-12-18 13:00:02 -0500

    configuration object

--- src/wavefinder/gui/app.py
+++ src/wavefinder/gui/app.py
@@ -13,30 +13,29 @@ from ..functions.focus import Focuser
 from ..functions.position import Positioner
 from ..functions.writer import DataWriter
 from .camera_panel import CameraPanel
-from .config import Configurable
+from .config import Configuration
 from .function_panel import FunctionPanel
 from .motion_panel import MotionPanel
 from .utils import Cyclic, make_task
 
 
-class App(tk.Tk, Configurable):
+class App(tk.Tk):
     """Main graphical application"""
 
-    def __init__(self, config_file="config.toml"):
+    def __init__(self, config_filename="config.toml"):
         """Main graphical application
 
-        config_file: filename of configuration
+        config_filename: filename of configuration
         """
 
         # For Windows, we need to set the DPI awareness so it looks right
         if "Windows".casefold() in platform.platform().casefold():
-            ctypes.windll.shcore.SetProcessDpiAwareness(1) # type: ignore
+            ctypes.windll.shcore.SetProcessDpiAwareness(1)  # type: ignore
 
         super().__init__()
 
         # config
-        self.default_config()
-        self.set_config(config_file)
+        self.config = Configuration(config_filename)
 
         # task variables
         self.loop = asyncio.get_event_loop()
@@ -59,36 +58,47 @@ class App(tk.Tk, Configurable):
 
         # camera
         try:
-            self.camera = Camera(run_mode=self.camera_run_mode,
-                                 bits=self.camera_bits,
-                                 freq_mode=self.camera_freq_mode,
-                                 resolution=self.camera_resolution,
-                                 bin_mode=self.camera_bin_mode,
-                                 nBuffer=self.camera_nBuffer,
-                                 exposure_time=self.camera_exposure_time,
-                                 fps=self.camera_fps,
-                                 gain=self.camera_gain)
+            self.camera = Camera(
+                run_mode=self.config.camera_run_mode,
+                bits=self.config.camera_bits,
+                freq_mode=self.config.camera_freq_mode,
+                resolution=self.config.camera_resolution,
+                bin_mode=self.config.camera_bin_mode,
+                nBuffer=self.config.camera_nBuffer,
+                exposure_time=self.config.camera_exposure_time,
+                fps=self.config.camera_fps,
+                gain=self.config.camera_gain,
+            )
         except ValueError as e:
             print(e)
             self.camera = None
 
         # motion axes
         self.axes: dict[str, Axis] = {}
-        self.zaber_adapter = ZaberAdapter(self.zaber_ports, self.zaber_axis_names)
+        self.zaber_adapter = ZaberAdapter(
+            self.config.zaber_ports, self.config.zaber_axis_names
+        )
         self.axes.update(self.zaber_adapter.axes)
-        self.galil_adapter = GalilAdapter(self.galil_address, self.galil_axis_names,
-                                          self.galil_acceleration, self.galil_deceleration,
-                                          self.galil_move_speed, self.galil_home_speed,
-                                          self.galil_encoder_counts_per_degree,
-                                          self.galil_drive_counts_per_degree)
+        self.galil_adapter = GalilAdapter(
+            self.config.galil_address,
+            self.config.galil_axis_names,
+            self.config.galil_acceleration,
+            self.config.galil_deceleration,
+            self.config.galil_move_speed,
+            self.config.galil_home_speed,
+            self.config.galil_encoder_counts_per_degree,
+            self.config.galil_drive_counts_per_degree,
+        )
         self.axes.update(self.galil_adapter.axes)
 
         # set motion limits
-        for axis_name, limits in self.motion_limits.items():
+        for axis_name, limits in self.config.motion_limits.items():
             axis = self.axes.get(axis_name, None)
             if axis:
-                make_task(axis.set_limits(low_limit=limits["min"],
-                                          high_limit=limits["max"]), self.tasks)
+                make_task(
+                    axis.set_limits(low_limit=limits["min"], high_limit=limits["max"]),
+                    self.tasks,
+                )
 
         # add devices to cyclic tasks
         # NOTE self.camera may be None if camera is not found, so only add it if not None
@@ -99,13 +109,20 @@ class App(tk.Tk, Configurable):
     def make_functions(self):
         """Make function units"""
 
-        x_axis = self.axes.get(self.camera_x_axis, None)
-        y_axis = self.axes.get(self.camera_y_axis, None)
-        z_axis = self.axes.get(self.focus_axis, None)
+        x_axis = self.axes.get(self.config.camera_x_axis, None)
+        y_axis = self.axes.get(self.config.camera_y_axis, None)
+        z_axis = self.axes.get(self.config.focus_axis, None)
 
-        self.positioner = Positioner(self.camera, x_axis, y_axis, px_size=self.pixel_size)
-        self.focuser = Focuser(self.camera, z_axis, self.focus_points_per_pass,
-                               self.focus_frames_per_point, self.focus_minimum_move)
+        self.positioner = Positioner(
+            self.camera, x_axis, y_axis, px_size=self.config.pixel_size
+        )
+        self.focuser = Focuser(
+            self.camera,
+            z_axis,
+            self.config.focus_points_per_pass,
+            self.config.focus_frames_per_point,
+            self.config.focus_minimum_move,
+        )
         self.writer = DataWriter(self.camera, self.axes, self.positioner, self.focuser)
 
     def make_panels(self):
@@ -116,12 +133,14 @@ class App(tk.Tk, Configurable):
         self.motion_panel = MotionPanel(self, self.axes)
         self.motion_panel.grid(column=0, row=1, sticky=tk.NSEW)
 
-        self.function_panel = FunctionPanel(self, focuser=self.focuser, positioner=self.positioner)
+        self.function_panel = FunctionPanel(
+            self, focuser=self.focuser, positioner=self.positioner
+        )
         self.function_panel.grid(column=1, row=1, sticky=tk.NSEW)
 
         # pad all panels
         for f in self.winfo_children():
-            f.configure(padding="3 3 12 12") # type: ignore
+            f.configure(padding="3 3 12 12")  # type: ignore
             f.grid_configure(padx=3, pady=(10, 0))
 
         # add panels to cyclic tasks
@@ -132,14 +151,14 @@ class App(tk.Tk, Configurable):
 
         These will run forever.
         """
-        make_task(self.update_loop(self.interval), self.tasks, self.loop)
+        make_task(self.update_loop(self.config.interval), self.tasks, self.loop)
         for u in self.cyclics:
-            make_task(u.update_loop(self.interval), self.tasks, self.loop)
+            make_task(u.update_loop(self.config.interval), self.tasks, self.loop)
 
     def run(self):
         """Run the loop"""
         print("--- Starting Application ---")
-        self.protocol("WM_DELETE_WINDOW", self.close) # bind close
+        self.protocol("WM_DELETE_WINDOW", self.close)  # bind close
         try:
             self.loop.run_forever()
         except KeyboardInterrupt:
--- src/wavefinder/gui/config.py
+++ src/wavefinder/gui/config.py
@@ -1,13 +1,24 @@
-
 import tomllib
 
 from ..devices.MightexBufCmos import Camera
 
 
-class Configurable:
-    def default_config(self):
+class Configuration:
+    """Application configuration object"""
+
+    def __init__(self, config_filename: str) -> None:
+        """Application configuration object
+
+        Args:
+            config_filename: name of config file
+        """
+        self.set_defaults()
+        self.read_config_file(config_filename)
+
+    def set_defaults(self):
+        "Set configuration to default."
         # task defaults
-        self.interval = 1/60 # seconds
+        self.interval = 1 / 60  # seconds
 
         # camera defaults
         self.camera_run_mode = Camera.NORMAL
@@ -22,30 +33,49 @@ class Configurable:
         self.pixel_size = (3.75, 3.75)
 
         # motion defaults
-        self.zaber_ports = ["COM1", "COM2", "COM3", "COM4", "COM5",
-                            "COM6", "COM7", "COM8", "COM9", "COM10",
-                            "/dev/ttyUSB0", "/dev/ttyUSB1", "/dev/ttyUSB2", "/dev/ttyUSB3"]
+        self.zaber_ports = [
+            "COM1",
+            "COM2",
+            "COM3",
+            "COM4",
+            "COM5",
+            "COM6",
+            "COM7",
+            "COM8",
+            "COM9",
+            "COM10",
+            "/dev/ttyUSB0",
+            "/dev/ttyUSB1",
+            "/dev/ttyUSB2",
+            "/dev/ttyUSB3",
+        ]
         self.galil_address = "192.168.1.19"
-        self.zaber_axis_names = {"detector x":  {"sn": 33938,  "keyword": "detxpos"},
-                                 "detector y":  {"sn": 33937,  "keyword": "detypos"},
-                                 "detector z":  {"sn": 33939,  "keyword": "detzpos"},
-                                 "cfm2 x":      {"sn": 110098, "keyword": "cfm2xpos"},
-                                 "cfm2 y":      {"sn": 113059, "keyword": "cfm2ypos"}}
-        self.galil_axis_names = {"cfm1 azimuth":    {"ch": "A", "keyword": "cfm1az"},
-                                 "cfm1 elevation":  {"ch": "B", "keyword": "cfm1el"},
-                                 "cfm2 azimuth":    {"ch": "C", "keyword": "cfm2az"},
-                                 "cfm2 elevation":  {"ch": "D", "keyword": "cfm2el"}}
+        self.zaber_axis_names = {
+            "detector x": {"sn": 33938, "keyword": "detxpos"},
+            "detector y": {"sn": 33937, "keyword": "detypos"},
+            "detector z": {"sn": 33939, "keyword": "detzpos"},
+            "cfm2 x": {"sn": 110098, "keyword": "cfm2xpos"},
+            "cfm2 y": {"sn": 113059, "keyword": "cfm2ypos"},
+        }
+        self.galil_axis_names = {
+            "cfm1 azimuth": {"ch": "A", "keyword": "cfm1az"},
+            "cfm1 elevation": {"ch": "B", "keyword": "cfm1el"},
+            "cfm2 azimuth": {"ch": "C", "keyword": "cfm2az"},
+            "cfm2 elevation": {"ch": "D", "keyword": "cfm2el"},
+        }
         self.galil_acceleration = 2000000
         self.galil_deceleration = 2000000
-        self.galil_move_speed   =  100000
-        self.galil_home_speed   =    5000
+        self.galil_move_speed = 100000
+        self.galil_home_speed = 5000
         self.galil_encoder_counts_per_degree = 800
         self.galil_drive_counts_per_degree = 10000
-        self.motion_limits = {"detector z":     {"min":   0.0, "max": 15.0},
-                              "cfm1 azimuth":   {"min": -30.0, "max": 30.0},
-                              "cfm1 elevation": {"min": -30.0, "max": 30.0},
-                              "cfm2 azimuth":   {"min": -30.0, "max": 30.0},
-                              "cfm2 elevation": {"min": -30.0, "max": 30.0}}
+        self.motion_limits = {
+            "detector z": {"min": 0.0, "max": 15.0},
+            "cfm1 azimuth": {"min": -30.0, "max": 30.0},
+            "cfm1 elevation": {"min": -30.0, "max": 30.0},
+            "cfm2 azimuth": {"min": -30.0, "max": 30.0},
+            "cfm2 elevation": {"min": -30.0, "max": 30.0},
+        }
 
         # positioner defaults
         self.camera_x_axis = "detector x"
@@ -57,23 +87,22 @@ class Configurable:
         self.focus_frames_per_point = 3
         self.focus_minimum_move = 0.001
 
-    def set_config(self, config_file: str):
-        """Set config
-
-        Args
-            config_file: name of config file
+    def read_config_file(self, config_filename: str) -> None:
+        """Read configuration file, validate input, overwrite defaults.
+        
+        Args:
+            config_filename: name of config file
         """
-        c = None
         try:
-            with open(config_file, "rb") as f:
+            with open(config_filename, "rb") as f:
                 c = tomllib.load(f)
         except FileNotFoundError as e:
             print(f"Can't find config file {e.filename}, using defaults")
             return
         except tomllib.TOMLDecodeError as e:
-            print(f"Error loading config file {config_file}, using defaults\n{e}")
+            print(f"Error loading config file {config_filename}, using defaults\n{e}")
             return
-
+        
         try:
             if "app" in c:
                 if "update_rate" in c["app"]:
@@ -120,7 +149,7 @@ class Configurable:
                     if isinstance(c["camera"]["fps"], float):
                         self.camera_fps = c["camera"]["fps"]
                 if "gain" in c["camera"]:
-                    if c["camera"]["gain"] in range(6,42):
+                    if c["camera"]["gain"] in range(6, 42):
                         self.camera_gain = c["camera"]["gain"]
             if "motion" in c:
                 if "zaber" in c["motion"]:
@@ -136,10 +165,14 @@ class Configurable:
                             self.galil_address = c["motion"]["galil"]["address"]
                     if "acceleration" in c["motion"]["galil"]:
                         if c["motion"]["galil"]["acceleration"] > 0:
-                            self.galil_acceleration = c["motion"]["galil"]["acceleration"]
+                            self.galil_acceleration = c["motion"]["galil"][
+                                "acceleration"
+                            ]
                     if "deceleration" in c["motion"]["galil"]:
                         if c["motion"]["galil"]["deceleration"] > 0:
-                            self.galil_deceleration = c["motion"]["galil"]["deceleration"]
+                            self.galil_deceleration = c["motion"]["galil"][
+                                "deceleration"
+                            ]
                     if "move_speed" in c["motion"]["galil"]:
                         if c["motion"]["galil"]["move_speed"] > 0:
                             self.galil_move_speed = c["motion"]["galil"]["move_speed"]
@@ -148,16 +181,20 @@ class Configurable:
                             self.galil_home_speed = c["motion"]["galil"]["home_speed"]
                     if "encdr_cnts_deg" in c["motion"]["galil"]:
                         if c["motion"]["galil"]["encdr_cnts_deg"] > 0:
-                            self.galil_encoder_counts_per_degree = c["motion"]["galil"]["encdr_cnts_deg"]
+                            self.galil_encoder_counts_per_degree = c["motion"]["galil"][
+                                "encdr_cnts_deg"
+                            ]
                     if "drive_cnts_deg" in c["motion"]["galil"]:
                         if c["motion"]["galil"]["drive_cnts_deg"] > 0:
-                            self.galil_drive_counts_per_degree = c["motion"]["galil"]["drive_cnts_deg"]
+                            self.galil_drive_counts_per_degree = c["motion"]["galil"][
+                                "drive_cnts_deg"
+                            ]
                     if "axis_names" in c["motion"]["galil"]:
                         if isinstance(c["motion"]["galil"]["axis_names"], dict):
                             self.galil_axis_names = c["motion"]["galil"]["axis_names"]
                 if "limits" in c["motion"]:
-                        if isinstance(c["motion"]["limits"], dict):
-                            self.motion_limits = c["motion"]["limits"]
+                    if isinstance(c["motion"]["limits"], dict):
+                        self.motion_limits = c["motion"]["limits"]
             if "positioner" in c:
                 if "x_axis" in c["positioner"]:
                     if isinstance(c["positioner"]["x_axis"], str):
@@ -179,44 +216,8 @@ class Configurable:
                     if c["focuser"]["minimum_move"] > 0:
                         self.focus_minimum_move = c["focuser"]["minimum_move"]
         except Exception as e:
-            print(f"Error parsing config file {config_file}, using defaults\n{e}")
-            self.default_config()
+            print(f"Error parsing config file {config_filename}, using defaults\n{e}")
+            self.set_defaults()
 
-    def print_config(self):
-        """Print all the config vars"""
-        print(f"interval = {self.interval}")
-
-        # camera
-        print(f"camera_run_mode = {self.camera_run_mode}")
-        print(f"camera_bits = {self.camera_bits}")
-        print(f"camera_freq_mode = {self.camera_freq_mode}")
-        print(f"camer_resolution = {self.camera_resolution}")
-        print(f"camera_bin_mode = {self.camera_bin_mode}")
-        print(f"camer_nBuffer = {self.camera_nBuffer}")
-        print(f"camera_exposure_time = {self.camera_exposure_time}")
-        print(f"camera_fps = {self.camera_fps}")
-        print(f"camera_gain = {self.camera_gain}")
-        print(f"pixel_size = {self.pixel_size}")
-
-        # motion
-        print(f"zaber_ports = {self.zaber_ports}")
-        print(f"galil_address = {self.galil_address}")
-        print(f"zaber_axis_names = {self.zaber_axis_names}")
-        print(f"galil_axis_names = {self.galil_axis_names}")
-        print(f"galil_acceleration = {self.galil_acceleration}")
-        print(f"galil_deceleration = {self.galil_deceleration}")
-        print(f"galil_move_speed = {self.galil_move_speed}")
-        print(f"galil_home_speed = {self.galil_home_speed}")
-        print(f"galil_encoder_counts_per_degree = {self.galil_encoder_counts_per_degree}")
-        print(f"galil_drive_counts_per_degree = {self.galil_drive_counts_per_degree}")
-        print(f"motion_limits = {self.motion_limits}")
-
-        # positioner
-        print(f"camera_x_axis = {self.camera_x_axis}")
-        print(f"camera_y_axis = {self.camera_y_axis}")
-
-        # focuser
-        print(f"focus_axis = {self.focus_axis}")
-        print(f"focus_points_per_pass = {self.focus_points_per_pass}")
-        print(f"focus_frames_per_point = {self.focus_frames_per_point}")
-        print(f"focus_minimum_move = {self.focus_minimum_move}")
+    def __str__(self) -> str:
+        return str(self.__dict__)
