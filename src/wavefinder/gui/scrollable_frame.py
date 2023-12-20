cd7561d09cbf6da51d5455984058ddfb0597746d
Cem Onyuksel <cem.onyuksel@cfa.harvard.edu>
2023-12-20 17:31:00 -0500

    scrollbars disappear when not needed

--- src/wavefinder/gui/scrollable_frame.py
+++ src/wavefinder/gui/scrollable_frame.py
@@ -12,27 +12,55 @@ class ScrollableContainer(ttk.Frame):
     def __init__(self, container, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
 
-        canvas = tk.Canvas(self)
-        scrollbar_v = ttk.Scrollbar(self, orient=tk.VERTICAL, command=canvas.yview)
-        scrollbar_h = ttk.Scrollbar(self, orient=tk.HORIZONTAL, command=canvas.xview)
-        sizegrip = ttk.Sizegrip(self)
-        self.frame = ttk.Frame(canvas)
+        self.canvas = tk.Canvas(self)
+        self.scrollbar_v = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.canvas.yview)
+        self.scrollbar_h = ttk.Scrollbar(self, orient=tk.HORIZONTAL, command=self.canvas.xview)
+        self.sizegrip = ttk.Sizegrip(self)
+        self.frame = ttk.Frame(self.canvas)
 
         # place frame inside canvas
-        canvas.create_window((0, 0), window=self.frame, anchor="nw")
+        self.window = self.canvas.create_window((0, 0), window=self.frame, anchor="nw")
 
-        # make canvas scale with frame size
-        self.frame.bind(
-            "<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
-        )
+        # make canvas scale with frame size and visa versa
+        self.frame.bind("<Configure>", self.onFrameConfigure)
+        self.canvas.bind("<Configure>", self.onCanvasConfigure)
         # make scrollbars match scroll position
-        canvas.configure(yscrollcommand=scrollbar_v.set)
-        canvas.configure(xscrollcommand=scrollbar_h.set)
+        self.canvas.configure(yscrollcommand=self.scrollbar_v.set)
+        self.canvas.configure(xscrollcommand=self.scrollbar_h.set)
 
         # place elements, canvas takes all extra space
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
-        scrollbar_v.grid(row=0, column=1, sticky=tk.NS)
-        scrollbar_h.grid(row=1, column=0, sticky=tk.EW)
-        sizegrip.grid(row=1, column=1)
-        canvas.grid(row=0, column=0, sticky=tk.NSEW)
+        self.scrollbar_v.grid(row=0, column=1, sticky=tk.NS)
+        self.scrollbar_h.grid(row=1, column=0, sticky=tk.EW)
+        self.sizegrip.grid(row=1, column=1)
+        self.canvas.grid(row=0, column=0, sticky=tk.NSEW)
+
+    def onFrameConfigure(self, event):
+        #Reset the scroll region to encompass the inner frame
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    def onCanvasConfigure(self, event):
+        #Resize the inner frame to match the canvas
+        minWidth = self.frame.winfo_reqwidth()
+        minHeight = self.frame.winfo_reqheight()
+
+        if self.winfo_width() >= minWidth:
+            newWidth = self.winfo_width()
+            #Hide the scrollbar when not needed
+            self.scrollbar_h.grid_remove()
+        else:
+            newWidth = minWidth
+            #Show the scrollbar when needed
+            self.scrollbar_h.grid()
+
+        if self.winfo_height() >= minHeight:
+            newHeight = self.winfo_height()
+            #Hide the scrollbar when not needed
+            self.scrollbar_v.grid_remove()
+        else:
+            newHeight = minHeight
+            #Show the scrollbar when needed
+            self.scrollbar_v.grid()
+
+        self.canvas.itemconfig(self.window, width=newWidth, height=newHeight)
